49a5116e54da6f39b488e024b2df5b31782482c6
BoronII <baranserajel@gmail.coml>
2020-12-27 18:31:37 -0500

    progression on preprocessing script

---
+++ scripts/Preprocessor.py
@@ -0,0 +1,24 @@
+
+import pandas as pd
+
+from pathlib import Path
+
+
+# All of these will be implemented as methods in the class
+
+# Common steps in preprocessing:
+# 1. Drop cols (list of to_drop columns)
+# 2. Drop rows that have nan values in a particular column
+# 3. Rename entries in a column from a dict
+# 4. Rename columns from a dict
+# 5. Functionality to use the same dict for a list of columns
+# 6. Save processed data frames
+
+# Tests
+# 1. check to make sure that columns have Nans in the same places before and after renaming
+
+class Preprocessor:
+	
+
+
+
